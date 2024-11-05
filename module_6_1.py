c691a33b6936a96511b285c89b780a3f9a39cecb
Danil019 <100717092+Danil019@users.noreply.github.com>
2024-11-05 15:10:55 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -1,22 +1,24 @@
 class Animal:
+    alive = True  # Атрибут класса
+    fed = False   # Атрибут класса
+
     def __init__(self, name):
-        self.name = name
-        self.alive = True
-        self.fed = False
+        self.name = name  # Атрибут экземпляра
 
     def eat(self, food):
-        if food.edible:  # Проверяем съедобность конкретного объекта
-            self.fed = True
+        if food.edible:  # Проверяем съедобность переданного объекта
+            Animal.fed = True  # Обновляем атрибут класса fed
             print(f"{self.name} съел {food.name}")
         else:
-            self.alive = False
+            Animal.alive = False  # Обновляем атрибут класса alive
             print(f"{self.name} не стал есть {food.name}")
 
 
 class Plant:
-    def __init__(self, name, edible=False):
-        self.name = name
-        self.edible = edible  # Определяем съедобность для каждого объекта
+    edible = False  # Атрибут класса
+
+    def __init__(self, name):
+        self.name = name  # Атрибут экземпляра
 
 
 class Mammal(Animal):
@@ -29,23 +31,27 @@ class Predator(Animal):
 
 class Flower(Plant):
     def __init__(self, name):
-        super().__init__(name, edible=False)  # Цветы несъедобны
+        super().__init__(name)
+        self.edible = False  # Оставляем несъедобным
 
 
 class Fruit(Plant):
     def __init__(self, name):
-        super().__init__(name, edible=True)  # Фрукты съедобны
+        super().__init__(name)
+        self.edible = True  # Делаем съедобным
 
 
+# Проверка работы программы
 a1 = Predator("Волк с Уолл-Стрит")
 a2 = Mammal("Хатико")
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
+p1 = Flower("Цветик семицветик")
+p2 = Fruit("Заводной апельсин")
+
 print(a1.name)
 print(p1.name)
-print(a1.alive)
-print(a2.fed)
+print(Animal.alive)
+print(Animal.fed)
 a1.eat(p1)
 a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
+print(Animal.alive)
+print(Animal.fed)
